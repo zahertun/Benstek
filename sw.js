42d416f29bf686b197029d6f2a93403c344425b8
zahertun <zaher.chkt@gmail.com>
2026-04-24 11:45:04 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'benstek-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne le cache si trouvé, sinon fait la requête réseau
+        return response || fetch(event.request);
+      })
+  );
+});
